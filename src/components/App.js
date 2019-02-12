fe284c272378f6811dd0720dc973a6656caf1fb2
Wizary <wizaryn@gmail.com>
2019-02-12 12:26:47 +0300

    boilerplate cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,12 @@ import importedComponent from 'react-imported-component';
 import Home from './Home';
 import Loading from './Loading';
 
-const AsyncDynamicPAge = importedComponent(
-  () => import(/* webpackChunkName:'DynamicPage' */ './DynamicPage'),
-  {
-    LoadingComponent: Loading
-  }
-);
+// const AsyncDynamicPAge = importedComponent(
+//   () => import(/* webpackChunkName:'DynamicPage' */ './DynamicPage'),
+//   {
+//     LoadingComponent: Loading
+//   }
+// );
 const AsyncNoMatch = importedComponent(
   () => import(/* webpackChunkName:'NoMatch' */ './NoMatch'),
   {
@@ -23,8 +23,7 @@ const App = () => {
     <Router>
       <div>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/dynamic" component={AsyncDynamicPAge} />
+          <Route exact path="/" component={Home} />          
           <Route component={AsyncNoMatch} />
         </Switch>
       </div>
--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Icon } from 'semantic-ui-react';
 
-import Layout from './Layout';
-
 const NoMatch = () => {
   return (
-    <Layout>
+    <React.Fragment>
       <Icon name="minus circle" size="big" />
       <strong>Page not found!</strong>
-    </Layout>
+    </React.Fragment>
   );
 };
 
